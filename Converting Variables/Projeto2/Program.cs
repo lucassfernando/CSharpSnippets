1c28a3826c950a67a2161344de7669167900aeea
Lucas Fernando <89320727+lucassfernando@users.noreply.github.com>
2023-05-12 17:37:15 -0300

    Converting Variables
    Converting Variables

---
+++ Converting Variables/Projeto2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Digite: ");
+            string Idade = Console.ReadLine();
+            int IdadeInt = Convert.ToInt32(Idade);
+
+            Console.WriteLine(IdadeInt);
+
+            string Tecla = Console.ReadKey(true).KeyChar.ToString();
+            Console.WriteLine("Tecla pressionada: " + Tecla);
+            Console.ReadKey();
+
+        }
+    }
+}
