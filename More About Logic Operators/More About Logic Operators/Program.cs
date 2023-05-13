c056c40d97e44b7d77e73aa13a65224137cd6a90
Lucas Fernando <89320727+lucassfernando@users.noreply.github.com>
2023-05-13 09:33:23 -0300

    More About Logic Operators
    More About Logic Operators

---
+++ More About Logic Operators/More About Logic Operators/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace More_About_Logic_Operators
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            bool Var1 = true;
+            bool Var2 = false;
+            bool ResultadoOu1 = Var1 | Var2;
+            bool ResultadoOu2 = Var1 || Var2;  //Utilizar assim o resultado é o mesmo, porém ele é mais rápido
+
+            bool ResultadoE1 = Var1 & Var2;
+            bool ResultadoE2 = Var1 && Var2;   //Utilizar assim o resultado é o mesmo, porém ele é mais rápido
+
+        }
+    }
+}
