14b524442a1e9c5d928fa5d6fd9224e12b56dc5c
Lucas Fernando <89320727+lucassfernando@users.noreply.github.com>
2023-05-27 10:53:04 -0300

    Loops of Repetition
    Loops of Repetition

---
+++ Loops of Repetition/Loops of Repetition/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loops_of_Repetition
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //for
+            for(int i = 0; i < 10;  i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            //while
+            int j = 0;
+            while(j < 20)
+            {
+                Console.WriteLine("oi");
+                j += 5;
+            }
+
+            //do...while
+            int u = 0;
+            do
+            {
+                Console.WriteLine("ola");
+            } while(u < 0);
+
+            //foreach
+            string[] nomes = { "Lucas", "Leo", "Carlos" };
+            foreach(string nome in nomes)
+            {
+                Console.WriteLine(nome);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
