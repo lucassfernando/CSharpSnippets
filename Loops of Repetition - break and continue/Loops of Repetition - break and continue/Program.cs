187235646b02a7447df3e2b1236d7ef679b74f06
Lucas Fernando <89320727+lucassfernando@users.noreply.github.com>
2023-05-28 10:13:44 -0300

    Loops of Repetition - break and continue
    Loops of Repetition - break and continue

---
+++ Loops of Repetition - break and continue/Loops of Repetition - break and continue/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Loops_of_Repetition___break_and_continue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Pessione C para continuar");
+                Console.WriteLine("Pressione B para parar");
+                string tecla = Console.ReadKey(true).KeyChar.ToString();
+
+                if( tecla == "c" )
+                {
+                    Console.WriteLine("Continuando...");
+                    continue;
+
+                } 
+                else if( tecla == "b" )
+                {
+                    Console.WriteLine("Parando...");
+                    Thread.Sleep(2000); 
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Tecla incorreta");
+                }
+            }
+
+        }
+    }
+}
