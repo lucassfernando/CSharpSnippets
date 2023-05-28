78c15f89f5d514b955c40116e056a25d5e084728
Lucas Fernando <89320727+lucassfernando@users.noreply.github.com>
2023-05-28 10:14:37 -0300

    Practicing - Calculate Factorial
    Practicing - Calculate Factorial

---
+++ Projects/Practicing - Rental Project/Practicing - Calculate Factorial/Practicing - Calculate Factorial/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practicing___Calculate_Factorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um número: ");
+            Int64 numeroInseridoTotal = Convert.ToInt64(Console.ReadLine());
+            Int64 numeroInserido = numeroInseridoTotal;
+            Int64 fatorial = 1;
+
+            while (numeroInserido > 0)
+            {
+                fatorial = fatorial * numeroInserido;
+                numeroInserido = numeroInserido - 1;
+                Console.WriteLine(fatorial);
+            }
+
+            if (numeroInserido >= 0)
+            {
+                Console.WriteLine("Fatorial(" + numeroInseridoTotal + ") =" + " " + fatorial);
+            }
+            else
+            {
+                Console.WriteLine("Não existe fatorial para este numero!");
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
