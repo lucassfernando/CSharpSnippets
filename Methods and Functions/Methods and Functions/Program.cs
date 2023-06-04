14c796e51d00eb000d61b59e39d509e637a46fd3
Lucas Fernando <89320727+lucassfernando@users.noreply.github.com>
2023-06-03 21:42:27 -0300

    Methods and Functions
    Methods and Functions

---
+++ Methods and Functions/Methods and Functions/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods_and_Functions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MeuMetodo();
+            string meuNome = RetornaNome("Lucas", "Fernando");
+            Console.WriteLine(meuNome);
+            Console.ReadKey();
+        }
+
+        static void MeuMetodo()
+        {
+            Console.WriteLine("C#");
+        }
+
+        static string RetornaNome(string nome, string sobrenome)
+        {
+            return nome + " " + sobrenome;
+        }
+    }
+}
