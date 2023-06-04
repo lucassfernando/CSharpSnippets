dcf7f6fc4728a4af23f0bc724a2173de4f21c5b0
Lucas Fernando <89320727+lucassfernando@users.noreply.github.com>
2023-06-03 21:42:58 -0300

    Structs
    Structs

---
+++ Structs/Structs/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Structs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product mouse = new Product(1, "Mouse", 100);
+
+            Console.WriteLine(mouse.Id);
+            Console.WriteLine(mouse.Nome);
+            Console.WriteLine(mouse.Preco);
+
+            Console.ReadKey();
+        }
+    }
+
+    struct Product
+    {
+        public Product(int id, string nome, double preco)
+        {
+            Id = id;
+            Nome = nome;
+            Preco = preco;
+        }
+
+        public int Id;
+        public string Nome;
+        public double Preco;
+        public double PrecoEmDolar(double dolar)
+        {
+            return Preco * dolar;
+        }
+
+    }
+}
